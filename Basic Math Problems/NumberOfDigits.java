605566f7178b7c73e0ea87081abfecf20ef17043
ashokkumar4224 <ashokkumarnarisetti96@gmail.com>
2026-01-01 23:14:16 +0530

    Added solution for Reversing the Number

--- Basic Math Problems/NumberOfDigits.java
+++ Basic Math Problems/NumberOfDigits.java
@@ -1,14 +1,5 @@
 import java.util.Scanner;
-/*
-Problem: Count Number of Digits
-Approach:
-- If the number is 0, digit count is 1
-- Otherwise, repeatedly divide the number by 10
-- Count how many times division happens
 
-Time Complexity: O(n) where n is number of digits
-Space Complexity: O(1)
-*/
 public class NumberOfDigits {
 
     public static int countDigits(long n) {
---
+++ Basic Math Problems/ReverseTheNumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+/*
+Problem = Reversing the Given Number.
+Approach:
+  - Extract the Last digit using Modulo.
+  - Build Reverse value by multiplying existing value by 10.
+  - Remove last digit by Division.
+Time Complexity = O(n).
+Space Complexity = O(1).
+*/ 
+public class ReverseTheNumber {
+    public static long reverseNumber(long n){
+    long rev = 0;
+      while(n > 0){
+        long lastDigit = n%10;
+        rev = (rev*10)+lastDigit;
+        n = n / 10;
+      }
+      return rev;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.print(reverseNumber(n));
+    }
+}
